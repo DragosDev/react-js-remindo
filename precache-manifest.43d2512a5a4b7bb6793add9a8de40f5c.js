d0cce100ae20f1c6140bed5874eba8fa4ced5cd5
DragosDev <dragosmotrea0@gmail.com>
2021-06-28 15:24:07 +0100

    Updates

---
+++ precache-manifest.43d2512a5a4b7bb6793add9a8de40f5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cebf35e6ed1dc71848204c1afa0f2a0",
+    "url": "/react-jsremindo/index.html"
+  },
+  {
+    "revision": "b94fcffffdfed64baf45",
+    "url": "/react-jsremindo/static/css/main.db1fd539.chunk.css"
+  },
+  {
+    "revision": "a9126082705d5b5d431b",
+    "url": "/react-jsremindo/static/js/2.daefb4dd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-jsremindo/static/js/2.daefb4dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b94fcffffdfed64baf45",
+    "url": "/react-jsremindo/static/js/main.9d9ddd72.chunk.js"
+  },
+  {
+    "revision": "885118e8f5f5448ff96d",
+    "url": "/react-jsremindo/static/js/runtime-main.254579c9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/habbit-reminder/precache-manifest.dbc415c695f2bd6154707ed1d846d1d3.js"
+  "/react-jsremindo/precache-manifest.43d2512a5a4b7bb6793add9a8de40f5c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/habbit-reminder/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-jsremindo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
